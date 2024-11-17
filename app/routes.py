77d0abbe23a602f067705e60f7a83e85a618c235
ExOwLcIsT <demchenkovlad80@gmail.com>
2024-11-17 13:18:27 +0200

    7,9

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from app.forms import ClientForm
-from datetime import datetime ,timedelta
+from datetime import datetime, timedelta
 from decorators.role_decorator import role_decorator
 # Ініціалізація Flask-додатка
 app = Flask(__name__)
@@ -13,15 +13,20 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['DatingAgency_Local']
 
 # Головна сторінка
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
 # Сторінка клієнтів із фільтрацією, сортуванням та пагінацією
+
+
 @app.route("/clients/<int:page>")
 def clients(page=1):
     per_page = 5
@@ -43,19 +48,23 @@ def clients(page=1):
     query["age"] = {"$gte": age_min, "$lte": age_max}
 
     # Отримання клієнтів із бази
-    clients_list = list(db.clients.find(query).sort(sort_by, 1).skip(skip).limit(per_page))
+    clients_list = list(db.clients.find(query).sort(
+        sort_by, 1).skip(skip).limit(per_page))
     total_clients = db.clients.count_documents(query)
     total_pages = (total_clients + per_page - 1) // per_page
 
     return render_template("clients.html", clients=clients_list, page=page, total_pages=total_pages, sort_by=sort_by, gender=gender, zodiac_sign=zodiac_sign, age_min=age_min, age_max=age_max)
 
 # Додавання нового клієнта
+
+
 @app.route("/add_client", methods=["GET", "POST"])
 @role_decorator("operator")
 def add_client():
     form = ClientForm()
     if form.validate_on_submit():
-        registration_date = datetime.combine(form.registration_date.data, datetime.min.time())
+        registration_date = datetime.combine(
+            form.registration_date.data, datetime.min.time())
         new_client = {
             "gender": form.gender.data,
             "registration_date": registration_date,
@@ -78,7 +87,6 @@ def add_client():
         flash("Client added successfully!", "success")
         return redirect(url_for("clients", page=1))
     return render_template("add_client.html", form=form)
-
 
 
 # Редагування клієнта
@@ -110,7 +118,8 @@ def edit_client(client_id):
                 "max_weight": request.form.get("partner_max_weight"),
             },
         }
-        db.clients.update_one({"_id": ObjectId(client_id)}, {"$set": updated_client})
+        db.clients.update_one({"_id": ObjectId(client_id)}, {
+                              "$set": updated_client})
         flash("Client updated successfully!", "success")
         return redirect(url_for("clients", page=1))
 
@@ -124,6 +133,8 @@ def edit_client(client_id):
     return render_template("edit_client.html", form=form, client=client)
 
 # Видалення клієнта
+
+
 @app.route("/delete_client/<client_id>", methods=["DELETE"])
 @role_decorator("owner")
 def delete_client(client_id):
@@ -132,6 +143,8 @@ def delete_client(client_id):
     return redirect(url_for("clients", page=1))
 
 # Перегляд детальної інформації про клієнта
+
+
 @app.route("/view_client/<client_id>")
 def view_client(client_id):
     client = db.clients.find_one({"_id": ObjectId(client_id)})
@@ -141,6 +154,8 @@ def view_client(client_id):
     return render_template("view_client.html", client=client)
 
 # Сторінка зустрічей із пагінацією
+
+
 @app.route("/meetings/<int:page>")
 def meetings(page=1):
     per_page = 5
@@ -151,8 +166,10 @@ def meetings(page=1):
 
     # Завантажуємо деталі клієнтів для кожної зустрічі
     for meeting in meetings_list:
-        meeting["client1"] = db.clients.find_one({"_id": ObjectId(meeting["client1_id"])})
-        meeting["client2"] = db.clients.find_one({"_id": ObjectId(meeting["client2_id"])})
+        meeting["client1"] = db.clients.find_one(
+            {"_id": ObjectId(meeting["client1_id"])})
+        meeting["client2"] = db.clients.find_one(
+            {"_id": ObjectId(meeting["client2_id"])})
 
     total_meetings = db.meetings.count_documents({})
     total_pages = (total_meetings + per_page - 1) // per_page
@@ -160,6 +177,8 @@ def meetings(page=1):
     return render_template("meetings.html", meetings=meetings_list, page=page, total_pages=total_pages)
 
 # Додавання нової зустрічі
+
+
 @app.route("/add_meeting", methods=["GET", "POST"])
 @role_decorator("operator")
 def add_meeting():
@@ -168,7 +187,7 @@ def add_meeting():
         client1_id = request.form.get("client1_id")
         client2_id = request.form.get("client2_id")
         date_str = request.form.get("date")
-        
+
         try:
             # Перетворення дати з рядка у формат datetime
             date = datetime.strptime(date_str, "%Y-%m-%d")
@@ -193,12 +212,16 @@ def add_meeting():
     return render_template("add_meeting.html", clients=clients_list)
 
 # Архів зустрічей
+
+
 @app.route("/archive")
 def archive():
     archive_list = list(db.archive.find())
     return render_template("archive.html", archive=archive_list)
 
 # Архівування зустрічі
+
+
 @app.route("/archive_meeting/<meeting_id>")
 def archive_meeting(meeting_id):
     meeting = db.meetings.find_one({"_id": ObjectId(meeting_id)})
@@ -213,6 +236,8 @@ def archive_meeting(meeting_id):
     return redirect(url_for("archive"))
 
 # Пошук клієнтів
+
+
 @app.route("/search_clients", methods=["GET", "POST"])
 def search_clients():
     if request.method == "POST":
@@ -234,6 +259,8 @@ def search_clients():
     return render_template("search_clients.html", clients=[])
 
 # Сторінка перегляду клієнтів за кварталом
+
+
 @app.route("/clients_by_quarter", methods=["GET"])
 def clients_by_quarter():
     quarter = request.args.get("quarter", None)
@@ -273,6 +300,7 @@ def clients_by_quarter():
 
     return render_template("clients_by_quarter.html", clients=clients_in_quarter, quarter=quarter)
 
+
 @app.route("/edit_meeting/<meeting_id>", methods=["GET", "POST"])
 @role_decorator("admin")
 def edit_meeting(meeting_id):
@@ -307,7 +335,8 @@ def edit_meeting(meeting_id):
             "status": status
         }
 
-        db.meetings.update_one({"_id": ObjectId(meeting_id)}, {"$set": updated_meeting})
+        db.meetings.update_one({"_id": ObjectId(meeting_id)}, {
+                               "$set": updated_meeting})
 
         # Якщо статус змінено на "completed", перенесення в архів
         if status == "completed":
@@ -324,12 +353,14 @@ def edit_meeting(meeting_id):
 
     return render_template("edit_meeting.html", meeting=meeting, clients=clients_list)
 
+
 @app.route("/delete_meeting/<meeting_id>", methods=["DELETE"])
 @role_decorator("owner")
 def delete_meeting(meeting_id):
     db.meetings.delete_one({"_id": ObjectId(meeting_id)})
-    return jsonify({"message" : "meeting deleted successfully"})
-    
+    return jsonify({"message": "meeting deleted successfully"})
+
+
 @app.route("/resolved_pairs")
 def resolved_pairs():
     # Отримати всі пари з архіву
@@ -337,8 +368,10 @@ def resolved_pairs():
 
     # Оновити кожну пару з деталями клієнтів
     for pair in pairs:
-        pair["client1"] = db.clients.find_one({"_id": ObjectId(pair["client1_id"])})
-        pair["client2"] = db.clients.find_one({"_id": ObjectId(pair["client2_id"])})
+        pair["client1"] = db.clients.find_one(
+            {"_id": ObjectId(pair["client1_id"])})
+        pair["client2"] = db.clients.find_one(
+            {"_id": ObjectId(pair["client2_id"])})
 
     # Логування для перевірки даних
     print(f"Знайдено {len(pairs)} пар у архіві.")
@@ -346,14 +379,15 @@ def resolved_pairs():
         print(pair)
 
     return render_template("resolved_pairs.html", pairs=pairs)
-from datetime import datetime, timedelta
+
 
 @app.route("/meetings_current_month", methods=["GET"])
 def meetings_current_month():
     """Відображення зустрічей на поточний місяць."""
     today = datetime.now()
     start_date = today.replace(day=1)
-    end_date = (start_date + timedelta(days=31)).replace(day=1) - timedelta(seconds=1)
+    end_date = (start_date + timedelta(days=31)
+                ).replace(day=1) - timedelta(seconds=1)
 
     # Отримуємо зустрічі за поточний місяць
     meetings = list(db.meetings.find({
@@ -362,12 +396,14 @@ def meetings_current_month():
 
     return render_template("meetings_month.html", meetings=meetings, month="current")
 
+
 @app.route("/meetings_next_month", methods=["GET"])
 def meetings_next_month():
     """Відображення зустрічей на наступний місяць."""
     today = datetime.now()
     start_date = (today.replace(day=1) + timedelta(days=31)).replace(day=1)
-    end_date = (start_date + timedelta(days=31)).replace(day=1) - timedelta(seconds=1)
+    end_date = (start_date + timedelta(days=31)
+                ).replace(day=1) - timedelta(seconds=1)
 
     # Отримуємо зустрічі за наступний місяць
     meetings = list(db.meetings.find({
@@ -376,11 +412,12 @@ def meetings_next_month():
 
     return render_template("meetings_month.html", meetings=meetings, month="next")
 
+
 @app.route("/clients_by_period", methods=["GET"])
 def clients_by_period():
     # Поточна дата
     today = datetime.now()
-    
+
     # Отримання параметра періоду
     period = request.args.get("period", "last_month")
 
@@ -401,12 +438,16 @@ def clients_by_period():
         return redirect(url_for("index"))
 
     # Запит до бази даних
-    clients = list(db.clients.find({"registration_date": {"$gte": start_date, "$lt": end_date}}))
+    clients = list(db.clients.find(
+        {"registration_date": {"$gte": start_date, "$lt": end_date}}))
 
     # Логування
-    print(f"Знайдено {len(clients)} клієнтів за період {period}: {start_date} - {end_date}")
+    print(f"Знайдено {len(clients)} клієнтів за період {
+          period}: {start_date} - {end_date}")
 
     return render_template("clients_by_period.html", clients=clients, period=period)
+
+
 @app.route("/non_missing_partners", methods=["GET"])
 def non_missing_partners():
     # Отримання статі з параметрів
@@ -422,7 +463,8 @@ def non_missing_partners():
     # Фільтрація клієнтів, які не пропустили жодної зустрічі
     result = []
     for client in clients:
-        meetings = list(db.meetings.find({"$or": [{"client1_id": str(client["_id"])}, {"client2_id": str(client["_id"])}]}))
+        meetings = list(db.meetings.find(
+            {"$or": [{"client1_id": str(client["_id"])}, {"client2_id": str(client["_id"])}]}))
         if all(meeting["status"] == "completed" for meeting in meetings):
             result.append(client)
 
@@ -430,6 +472,8 @@ def non_missing_partners():
     print(f"Знайдено {len(result)} клієнтів, які не пропустили зустрічей.")
 
     return render_template("non_missing_partners.html", partners=result, gender=gender)
+
+
 @app.route("/matching_grooms", methods=["GET"])
 def matching_grooms():
     # Отримати всіх жінок із бази
@@ -465,11 +509,12 @@ def api_login():
     password = data.get('password')
     print(login)
     print(password)
-    user = db.Keys.find_one({"login":login, "password" : password})
+    user = db.Keys.find_one({"login": login, "password": password})
     if user is not None:
         return jsonify({"role": user['role']}), 200
     else:
         return jsonify({"error": "Invalid login or password"}), 401
+
 
 @app.route('/api/register', methods=['POST'])
 @role_decorator("admin")
@@ -478,27 +523,32 @@ def register():
     login = data.get('login')
     password = data.get('password')
     role = data.get('role')
-    current_role = request.cookies.get('role')    
-    user = db.Keys.find_one({"login":login})
+    current_role = request.cookies.get('role')
+    user = db.Keys.find_one({"login": login})
     if user is not None:
         return jsonify({"error": "User already exists"}), 409
     if (role == "admin" and current_role != "owner"):
         return jsonify({"error": "Only owner can add admin"}), 403
-    db.Keys.insert_one({"login" : login, "password" : password, "role" : role})
+    db.Keys.insert_one({"login": login, "password": password, "role": role})
     return jsonify({"message": "User registered successfully"}), 201
+
 
 @app.route('/api/forgot-password', methods=['GET'])
 def forgot_password():
     login = request.args.get('login')
-    user = db.Keys.find_one({"login":login})
+    user = db.Keys.find_one({"login": login})
     if user is not None:
         return jsonify({"password": user['password']}), 200
     else:
         return jsonify({"error": "User not found"}), 404
+
+
 @app.route("/select_user", methods=["GET"])
 def select_user():
     users = list(db.clients.find())  # Отримати список користувачів
     return render_template("select_user.html", users=users)
+
+
 @app.route("/matches/<user_id>", methods=["GET"])
 def matches(user_id):
     user = db.clients.find_one({"_id": ObjectId(user_id)})
@@ -511,10 +561,22 @@ def matches(user_id):
     query = {
         "age": {"$gte": requirements.get("min_age", 0), "$lte": requirements.get("max_age", 120)},
         "height": {"$gte": requirements.get("min_height", 0), "$lte": requirements.get("max_height", 250)},
-       "weight": {"$gte": requirements.get("min_weight", 0), "$lte": requirements.get("max_weight", 300)},
-       "zodiac_sign": {"$in": requirements.get("zodiac_sign", [])},
-       "gender": "male" if user["gender"] == "female" else "female"
+        "weight": {"$gte": requirements.get("min_weight", 0), "$lte": requirements.get("max_weight", 300)},
+        "zodiac_sign": {"$in": requirements.get("zodiac_sign", [])},
+        "gender": "male" if user["gender"] == "female" else "female"
     }
-    
+
     matches = list(db.clients.find(query))
     return render_template("matches.html", user=user, matches=matches)
+
+
+@app.route('/canceled_meetings', methods=["GET"])
+def canceled_meetings():
+    result = list(db.meetings.find({"status": "cancelled"}))
+    return render_template("canceled_meetings.html", meetings=result)
+
+
+@app.route('/completed_meetings', methods=["GET"])
+def completed_meetings():
+    result = list(db.meetings.find({"status": "completed"}))
+    return render_template("completed_meetings.html", meetings=result)
